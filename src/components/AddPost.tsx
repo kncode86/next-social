d6aef2ce656be5917ce4e005f77004eca07aff47
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-04 19:06:41 +0000

    feat: create Clerk Webhook

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,11 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
-const AddPost = () => {
+const AddPost = async () => {
+
+    const {userId} = await auth();
+
     return (
         <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
             {/*AVATAR*/}
@@ -15,8 +20,12 @@ const AddPost = () => {
             {/*POST*/}
             <div className="flex-1">
                 {/*TEXT INPUT*/}
-                <div className="flex gap-4">
-                    <textarea placeholder="What's on your mind?" className="flex-1 bg-slate-100 rounded-lg p-2"></textarea>
+                <form action="" className="flex gap-4">
+                    <textarea 
+                        placeholder="What's on your mind?" 
+                        className="flex-1 bg-slate-100 rounded-lg p-2"
+                        name="description"
+                    ></textarea>
                     <Image 
                         src="/emoji.png"
                         alt=""
@@ -24,7 +33,8 @@ const AddPost = () => {
                         height={20}
                         className="w-5 h-5 cursor-pointer self-end"
                     />
-                </div>
+                    <button>Send</button>
+                </form>
 
                 {/*POST OPTIONS*/}
                 <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
