a4a5788d1201e5359ce1f27d39b9eb42525d7915
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-05 18:42:56 +0000

    feat: data fetching on Server Components

---
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+const UserInfoCardIntercation = ({
+    currentUserId,
+    userId,
+    isUserBlocked,
+    isFollowing,
+    isFollowingSent
+}:{
+    currentUserId:string;
+    userId:string;
+    isUserBlocked:boolean;
+    isFollowing:boolean;
+    isFollowingSent:boolean;
+}) => {
+
+    return (
+        <>
+            <form action="">
+                <button className="w-full bg-blue-500 text-white text-sm rounded-md p-2">
+                    {isFollowing
+                    ? "Following" 
+                    : isFollowingSent 
+                    ? "Friend Request Sent" 
+                    : "Follow"}
+                </button>
+            </form>
+            <form action="" className="self-end">
+                <span className="text-red-400 text-xs cursor-pointer">
+                        {isUserBlocked? "Unblock User" : "Block User"}
+                    </span>
+            </form>        
+        </>
+    )
+}
+
+export default UserInfoCardIntercation;
