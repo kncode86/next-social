cd20380a5120eff66e25749e602970e42afa6d11
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-11 21:31:50 +0000

    feat: create User-Stories

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,46 +1,42 @@
-import Image from "next/image";
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import StoryList from "./StoryList";
 
-const Stories = () => {
+const Stories = async () => {
+
+    const {userId:currentUserId} = await auth();
+
+    if (!currentUserId) return null;
+
+    const stories = await prisma.story.findMany({
+        where:{
+            expiresAt:{
+                gt: new Date(),
+            },
+            OR:[
+                {
+                    user:{
+                        followers:{
+                            some:{
+                                followerId: currentUserId,
+                            }
+                        }
+                    }
+                },
+                {
+                    userId: currentUserId,
+                }
+            ]
+        },
+        include:{
+            user:true,
+        }
+    });
+
     return (
         <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm scrollbar-hide">
             <div className="flex gap-8 w-max">
-                {/*STORY*/}
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
-                <div className="flex flex-col items-center gap-2 cursor-pointer">
-                    <Image src="https://images.pexels.com/photos/28594392/pexels-photo-28594392/free-photo-of-muveszi-lapos-kavealapanyag-sotet-feluleten.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width={80} height={80} className="w-20 h-20 rounded-full ring-2" />
-                    <span className="font-medium">Ricky</span>
-                </div>
+                <StoryList stories={stories} userId={currentUserId} />                
             </div>
         </div>
     )
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -292,3 +292,44 @@ export const addPost = async (formData:FormData, img:string) => {
         console.log(error);
     }
 }
+
+export const addStory = async (img:string) => {
+
+    const {userId} = await auth();
+
+    if(!userId){
+        throw new Error("User is not authenticated!");
+    }
+
+    try {
+        const existingStory = await prisma.story.findFirst({
+            where:{
+                userId
+            }
+        });
+
+        if (existingStory) {
+            await prisma.story.delete({
+                where:{
+                    id:existingStory.id,
+                }
+            });
+        }
+
+        const createdStory = await prisma.story.create({
+            data: {
+                userId,
+                img,
+                expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+            },
+            include:{
+                user:true,
+            }
+        });
+
+        return createdStory;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
