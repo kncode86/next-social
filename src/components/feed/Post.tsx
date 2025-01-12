664cd1df9a0d6d550e5f4e96dbfa218e76451821
kncode86 <kulcsar.norbert.86@gmail.com>
2025-01-12 10:28:58 +0000

    feat: create Delete User

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -2,10 +2,16 @@ import Image from "next/image";
 import Comments from "./Comments";
 import { Post as PostType, User } from "@prisma/client";
 import PostInteraction from "./PostInteraction";
+import { Suspense } from "react";
+import PostInfo from "./PostInfo";
+import { auth } from "@clerk/nextjs/server";
 
 type FeedPostType = PostType & {user:User} & {likes:[{userId:string}]} & {_count:{comments:number}}
 
-const Post = ({post}:{post:FeedPostType}) => {
+const Post = async ({post}:{post:FeedPostType}) => {
+
+    const {userId} = await auth();
+
     return (
         <div className="flex flex-col gap-4">
             {/*USER*/}
@@ -24,12 +30,7 @@ const Post = ({post}:{post:FeedPostType}) => {
                             : post.user.username
                         }</span>
                 </div>
-                <Image 
-                    src="/more.png"
-                    alt="" 
-                    width={16}
-                    height={16}
-                />
+                {userId === post.user.id && <PostInfo postId={post.id} />}
             </div>
 
             {/*DESCRIPTION*/}
@@ -48,12 +49,16 @@ const Post = ({post}:{post:FeedPostType}) => {
             </div>
 
             {/*INTERACTION*/}
-            <PostInteraction 
-                postId={post.id}
-                likes={post.likes.map(like => like.userId)}
-                commentNumber={post._count.comments}
-            />
-            <Comments postId={post.id} />
+            <Suspense fallback="Loading...">
+                <PostInteraction 
+                    postId={post.id}
+                    likes={post.likes.map(like => like.userId)}
+                    commentNumber={post._count.comments}
+                />
+            </Suspense>
+            <Suspense fallback="Loading...">
+                <Comments postId={post.id} />
+            </Suspense>     
         </div>
     )
 }
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -333,3 +333,23 @@ export const addStory = async (img:string) => {
         console.log(error);
     }
 }
+
+export const deletePost = async (postId:number) => {
+    const {userId} = await auth();
+
+    if(!userId){
+        throw new Error("User is not authenticated!");
+    }
+
+    try {
+        await prisma.post.delete({
+            where:{
+                id: postId,
+                userId,
+            }
+        });
+        revalidatePath("/");
+    } catch (error) {
+        console.log(error);
+    }
+}
